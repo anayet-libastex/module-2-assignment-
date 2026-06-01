180f1da63083d98581982d6c6752d78ae9f415d5
Anayet <anayethossain030@gmail.com>
2026-06-01 15:39:02 +0600

    package.json and vite.config.js file simple bug fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: '/module-2-assignment-/', 
+  base: '/module-2-assignment/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
